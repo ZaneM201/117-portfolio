26de68482e5ebdfa9354211b73135584d1dd328d
Zane Moon <zanem201@gmail.com>
2025-12-11 19:48:09 -0800

    Class 5 and Assignment 3

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+import certifi
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Create a secure Connection with certifi
+os.environ['SSL_CERT_FILE'] = certifi.where()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -121,6 +130,16 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Email Configuration
+# Backend Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def about_view(request):
@@ -6,3 +8,44 @@ def about_view(request):
 
 def experience_view(request):
     return render(request, 'pages/experience.html')
+
+def contact_view(request):
+    # Handle contact form submission
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        # Validate the form and collect data
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            
+            # Build the email content
+            message_body = (
+                f'You have a new email from your Portfolio webpage \n'
+                f'Name: {name}\n'
+                f'Email: {email}\n'
+                f'Message: {message}\n'
+            )
+            try:
+                send_mail(
+                    # Subject
+                    f'Email from Portfolio Website',
+                    # Message Body --> The user typed message
+                    message_body,
+                    email,  # From email (user's email)
+                    ['zanem201@gmail.com'],  # To email (your email)
+                )
+                # after sending the email
+                form = ContactForm()  # Reset the form
+                return render(request, 'pages/contact.html', { 'form': form})
+            except Exception as e:
+                print(f'Error sending email: {e}')
+                return render(request, 'pages/contact.html', {'form': form})
+        else:
+            print('Form is not valid')
+    else:
+        form = ContactForm()
+        return render(request, 'pages/contact.html', {'form': form})
+    
+    form = ContactForm()
+    return render(request, 'pages/contact.html', {'form': form})
